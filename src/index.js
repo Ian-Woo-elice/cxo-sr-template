4aee726d5d9bc6565f3585ad0141e43384f2caf6
Ian-Woo-elice <symphonie913@gmail.com>
2025-06-16 10:35:35 +0900

    Fix GH Pages setup

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import App from './App.js';
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
